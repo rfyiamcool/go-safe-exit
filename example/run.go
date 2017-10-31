3643e84f629dcae0219f12146770c78bd3952d29
rfyiamcool <rfyiamcool@163.com>
2017-10-31 17:16:34 +0800

    add example

---
+++ example/run.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	safe_exit "github.com/rfyiamcool/go-safe-exit"
+)
+
+func main(){
+	timeout := 3
+	EngineWaitGroup := safe_exit.NewControlGroup()
+
+	EngineWaitGroup.MakeSignal()
+	EngineWaitGroup.RecvSignal()
+
+	if EngineWaitGroup.WaitTimeout(timeout) {
+		log.Println("timeout")
+	} else {
+		log.Println("正常退出")
+	}
+
+}
